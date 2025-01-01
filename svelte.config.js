00ccb758533fdbdec4d91d42905e2ddb866ebcdc
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-01-02 05:51:32 +0700

    add base path configuration for GitHub Pages deployment in Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -11,7 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    paths: {
+      // If deploying to GitHub Pages, use the repository name as the base path
+      base: process.env.NODE_ENV === 'production' ? '/svelte-portfolio' : '',
+    },
 	}
 };
 
