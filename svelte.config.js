4dc1e6010050903252ec227cf82d64d47fff61f0
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-01-02 06:14:31 +0700

    configure Svelte adapter for static site generation and enable prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-      pages: 'build',  // This should match the deploy folder
-      assets: 'build', // Ensure this is the correct assets folder
-      fallback: null,  // Optional: Add fallback if needed (e.g., for single-page apps)
-    }),
+		adapter: adapter(),
     paths: {
       // If deploying to GitHub Pages, use the repository name as the base path
       base: process.env.NODE_ENV === 'production' ? '/svelte-portfolio' : '',
